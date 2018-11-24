dbb4f9e11a71ef06624800ac35204aad33754dc0
Daniel N. Lang <daniel.lang@velaia.de>
2018-11-24 20:13:03 +1100

    added orders to my_viewˆ

--- mypyramidapp/views.py
+++ mypyramidapp/views.py
@@ -1,6 +1,9 @@
 from pyramid.view import view_config
 
+from mypyramidapp.fake_data import get_orders
+
 
 @view_config(route_name='home', renderer="templates/mytemplate.pt")
 def my_view(request):
-    return {'project': 'myPyramidApp'}
+    orders = get_orders()
+    return {'project': 'myPyramidApp', 'orders': orders}
