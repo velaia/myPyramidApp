ed9aa2f2ed223581e11379cf563254eeb6de536a
Daniel N. Lang <daniel.lang@velaia.de>
2018-11-24 20:11:52 +1100

    added ordersˆ

---
+++ mypyramidapp/fake_data.py
@@ -0,0 +1,9 @@
+def get_orders():
+    return [
+        {'name': 'Cereal', 'price': 4.99},
+        {'name': 'Cheese', 'price': 2.15},
+        {'name': 'Milk', 'price': 6.99},
+        {'name': 'Oranges', 'price': 2.54},
+        {'name': 'Apples', 'price': 1.99},
+        {'name': 'Bread', 'price': 2.99},
+    ]
