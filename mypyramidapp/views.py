5c9f2767daa036f8d41125a72c07c69dcd671b2a
Daniel N. Lang <daniel.lang@velaia.de>
2018-11-24 20:19:05 +1100

    added orders to template file and added fs twitter feedˆ

--- mypyramidapp/views.py
+++ mypyramidapp/views.py
@@ -4,6 +4,6 @@ from mypyramidapp.fake_data import get_orders
 
 
 @view_config(route_name='home', renderer="templates/mytemplate.pt")
-def my_view(request):
+def my_view(_):
     orders = get_orders()
     return {'project': 'myPyramidApp', 'orders': orders}
